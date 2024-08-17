b81cec230a6ce7bce9a3eede2f10db7cc47ea5fe
mcrawf9394 <mjc9394@gmail.com>
2024-08-17 09:48:54 -0400

    Added jwt tokens and hid all of the secrets and such that I needed to hide.

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,19 @@
+import {Link, Route, Routes} from 'react-router-dom'
+import './stylesheets/App.css'
+function App() {
+  return (
+    <>
+      <nav className='navBar'>
+        <h1></h1>
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+      </nav>
+      <Routes>
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
